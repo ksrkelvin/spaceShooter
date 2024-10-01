8ea420f1f339494ae39ca75e8d18fc233d22acae
Kelvin Rodrigues <kelvin.rodrigues@ebinterchange.com.br>
2024-10-01 09:12:49 -0300

    Dev player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+    }
+
+    public void Inputs()
+    {
+        Move();
+
+    }
+
+    public void Move()
+    {
+
+        Vector2 newVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        newVelocity.Normalize();
+        rb.velocity = newVelocity * speed;
+
+
+    }
+
+}
